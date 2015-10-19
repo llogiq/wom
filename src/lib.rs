5180616001df21ced4d06d406a35c2a4ad0fbcc4
Andre Bogus <bogusandre@gmail.com>
2015-10-19 10:15:56 +0200

    Cargo-fied, add docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,60 @@
 //!
 //! This is a wrapper type to disallow reading from the wrapped type
 
-use std::convert::{AsRef, AsMut}
+use std::convert::{AsRef, AsMut};
 
 /// Make `T` Write-Only
-pub struct<T> Wom<T> {
+pub struct Wom<T> {
     #[doc(hidden)]
     _inner: T
 }
 
-impl<T: ?Sized> AsRef<T> for Wom<T> {
+impl<T> Wom<T> {
+    /// Transmute this value into the wrapped value
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///# use wom::wom;
+    /// assert_eq!(1, wom(1u32).into_inner());
+    /// ```
+    pub fn into_inner(self) -> T {
+        self._inner
+    }
+}
+
+/// Create a Wom handle on our `T`.
+pub fn wom<T>(t: T) -> Wom<T> {
+    Wom { _inner: t }
+}
+
+impl<T: Sized> AsRef<T> for Wom<T> {
+    /// Getting an immutable value out should panic.
+    /// # Examples
+    ///
+    /// ```should_panic
+    ///# use wom::wom;
+    /// wom(1u32).as_ref();
+    /// ```
     fn as_ref(&self) -> &T {
         panic!("readably referencing write-only memory");
     }
 }
 
-impl<T: ?Sized> AsMut<T> for Wom<T> {
+impl<T: Sized> AsMut<T> for Wom<T> {
+    /// Transmute this value into the wrapped value
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///# use wom::wom;
+    /// let x = [1u32];
+    /// let mut w = wom(x);
+    /// w.as_mut()[0] = 2;
+    /// assert_eq!(2, w.into_inner()[0]);
+    /// ```
     fn as_mut(&mut self) -> &mut T {
-        self._inner
+        &mut self._inner
     }
 }
 
